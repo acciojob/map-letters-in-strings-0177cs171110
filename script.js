692200a35b0439f9e25718a6232f84ca781deea5
Rahul Kumar <90969936+0177cs171110@users.noreply.github.com>
2024-03-12 14:55:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+function mapLetters(word) {
+    const letterMap = {};
+
+    // Iterate over the characters of the word
+    for (let i = 0; i < word.length; i++) {
+        const letter = word[i];
+
+        // If the letter is not already in the map, initialize an empty array for its indices
+        if (!letterMap.hasOwnProperty(letter)) {
+            letterMap[letter] = [];
+        }
+
+        // Add the index of the current occurrence of the letter to its array of indices
+        letterMap[letter].push(i);
+    }
+
+    return letterMap;
+}
